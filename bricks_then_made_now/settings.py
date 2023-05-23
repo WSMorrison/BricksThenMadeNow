b0b3506fc436ec2d9aad0f5b7eec491fba5f003d
WSMorrison <willimo@gmail.com>
2023-05-23 18:32:50 +0000

    Update favicon, style.css, and collect static files.

--- bricks_then_made_now/settings.py
+++ bricks_then_made_now/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-#DEBUG = 0
-
 
 ALLOWED_HOSTS = ['localhost']
 
@@ -154,6 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
