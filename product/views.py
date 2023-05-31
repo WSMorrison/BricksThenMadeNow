1378aa5de54ea81c68cb8370b275728b3f0cae17
WSMorrison <willimo@gmail.com>
2023-05-31 17:07:59 +0000

    Add view, url, and update index.html to display products. Inop at this time, probably an issue with the view.

--- product/views.py
+++ product/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Item
 
-# Create your views here.
+
+# Index page with all items.
+class AllItems(generic.ListView):
+    model = Item
+    queryset = Item.objects.all()
+    template_name = 'index.html'
+    # paginate_by = 10
