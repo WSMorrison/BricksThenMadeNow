452aa52cc7b2c4a048130eb36b38a9f1b50dab51
WSMorrison <willimo@gmail.com>
2023-06-01 15:20:54 +0000

    Add code to nav-bar.html hrefs and code to view so that user can select a category of items.

--- product/views.py
+++ product/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Item
+from .models import Item, Theme
 
 
 # Index page with all items.
 def all_items(request):
     items = Item.objects.all()
-    context = {'items': items,}
+    themes = None
+
+    if request.GET:
+        if 'theme' in request.GET:
+            themes = request.GET['theme'].split(',')
+            items = items.filter(item_theme__name__in=themes)
+            themes = Theme.objects.filter(name__in=themes)
+
+    context = {'items': items, 'current_theme': themes,}
+    print(themes)
 
     return render(request,'product/items.html', context)
