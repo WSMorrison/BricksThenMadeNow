a9f9ce2d1b0901f7a90344409a5b3b6bc7025dc5
WSMorrison <willimo@gmail.com>
2023-06-08 14:17:07 +0000

    Add code to settings and change product item model to save the product images to a media/images folder.

--- bricks_then_made_now/settings.py
+++ bricks_then_made_now/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
 
-ALLOWED_HOSTS = ['localhost', '8000-wsmorrison-bricksthenma-ck3p1ur53a0.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '8000-wsmorrison-bricksthenma-ck3p1ur53a0.ws-eu99.gitpod.io']
 
 
 # Application definition
@@ -153,8 +153,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
