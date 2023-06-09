35b2886795184438b56b8d591d8cf95559462cc5
WSMorrison <willimo@gmail.com>
2023-06-09 12:49:46 +0000

    Early steps in adding a sort and functionality.

--- product/views.py
+++ product/views.py
@@ -9,9 +9,10 @@ def all_items(request):
     items = Item.objects.all()
     query = None
     themes = None
+    sort = None
+    direction = None
 
     if request.GET:
-
         if 'theme' in request.GET:
             themes = request.GET['theme'].split(',')
             items = items.filter(item_theme__name__in=themes)
@@ -25,6 +26,17 @@ def all_items(request):
             queries = Q(item_name__icontains=query) | Q(item_description__icontains=query)
             items = items.filter(queries)
 
-    context = {'items': items, 'current_theme': themes,}
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            items = items.order_by(sortkey)
+
+    current_sort = f'{sort}_{direction}'
+
+    context = {'items': items, 'current_theme': themes, 'current_sort': current_sort}
 
     return render(request,'product/items.html', context)
