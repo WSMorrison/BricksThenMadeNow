aacaa0dad432d1ce5cfd5913bd6905fbf2f6b614
WSMorrison <willimo@gmail.com>
2023-06-27 11:01:44 +0000

    Add admin code for admin control panel.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Order, LineItem
 
-# Register your models here.
+
+class LineItemAdminInline(admin.TabularInline):
+    model = LineItem
+    readonly_fields = 'lineitem_total',
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (LineItemAdminInline,)
+    
+    readonly_fields = ('order_number',
+                       'date',
+                       'shipping_cost',
+                       'order_total',
+                       'grand_total',
+                       )
+
+    fields = ('order_number',
+              'date',
+              'full_name',
+              'email',
+              'phone_number',
+              'country',
+              'postcode',
+              'town_or_city',
+              'street_address1',
+              'street_address2',
+              'county',
+              'shipping_cost',
+              'order_total',
+              'grand_total',)
+
+    list_display = ('order_number',
+                    'date',
+                    'full_name',
+                    'order_total',
+                    'shipping_cost',
+                    'grand_total',
+                    )
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
