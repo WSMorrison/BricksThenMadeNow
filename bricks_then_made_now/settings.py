2b0a3e7705d8e1e05128cf9f42a8edf28fbc14a8
WSMorrison <willimo@gmail.com>
2023-07-07 17:15:51 +0000

    Heroku static and db files troubleshooting.

--- bricks_then_made_now/settings.py
+++ bricks_then_made_now/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = 0
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
