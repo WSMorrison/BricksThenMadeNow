1be60b749d28199676c47261b8d1952b6465e1ff
WSMorrison <willimo@gmail.com>
2023-07-10 20:12:41 +0000

    Improve formatting and styling for new_sku.html.

--- product/forms.py
+++ product/forms.py
@@ -9,7 +9,18 @@ from django.core.exceptions import ValidationError
 def pdf_url_ok(value):
     if value.endswith('.pdf') is False:
         if value.startswith('https://') is False:
-            raise forms.ValidationError('File must be secure .pdf')
+            raise forms.ValidationError('File must be secure .pdf.')
+
+
+def sku_number_ok(value):
+    if value != 1033:
+        if value !=1066:
+            if value !=1099:
+                raise forms.ValidationError('Must be valid Sku type.')
+
+def inventory_ok(value):
+    if value < 0:
+        raise forms.ValidationError('Must be positive inventory.')
 
 
 # New Item form
@@ -23,8 +34,7 @@ class ItemForm(forms.ModelForm):
                                   }))
 
     
-    item_instructions_url = forms.CharField(label='Downloadable .pdf link',
-                                            validators=[pdf_url_ok],
+    item_instructions_url = forms.CharField(validators=[pdf_url_ok],
                                             widget=forms.URLInput(attrs={
                                             'placeholder': 'https:// ... .pdf'
                                             })
@@ -33,19 +43,12 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = '__all__'
-        labels = {'item_name': 'Item name; use the original Lego set name:',    
-                  'item_theme': 'Choose theme:',
-                  'item_description': 'Brief item description; remember SEO keywords:',
-                  'item_old_url': 'Image hosted link:',
-                  'item_render_link': 'Render hosted link:',
-                  'item_modern_link': 'Modern, corporeal image, where available, hosted link:',
-                  'item_detail_url': 'Detail image hosted link:',
-                  'item_part_count': 'How many Lego bricks and elements are used? Exclude minifigs:'
-                  }
 
 
 # New Sku form
 class SkuForm(forms.ModelForm):
+
+    sku_number = forms.CharField(validators=[sku_number_ok])
 
     class Meta:
         model = Sku
